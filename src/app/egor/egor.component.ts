352d79b255616ce87c73432f886252d905d7d400
dimur <dimurg134@gmail.com>
2025-03-24 14:22:05 +0300

    add egor

---
+++ src/app/egor/egor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-egor',
+  imports: [],
+  templateUrl: './egor.component.html',
+  styleUrl: './egor.component.scss'
+})
+export class EgorComponent {
+
+}
